44ba15af752382f76c6c3178e6713de3eee538b8
visot <visotskiy.alexey@gmail.com>
2023-04-09 20:48:24 +0700

    Добавьте файлы проекта.

---
+++ Task15_1_4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task15_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "Патефон";
+            string word2 = "Магнитафон";
+
+            var count = word1.Intersect(word2).Count();
+            Console.WriteLine(count);
+        }
+    }
+}
