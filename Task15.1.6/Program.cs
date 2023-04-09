d248559975bbabf98b12c2d21169347b9301bb31
visot <visotskiy.alexey@gmail.com>
2023-04-09 20:56:01 +0700

    Задания 15.1.4-6

---
+++ Task15.1.6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task15_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Мама мыла раму, а папа лежал на диване.";
+            char[] except = new char[] { ',', '.', ';', ':', '?', '!' };
+
+            var newText= text.Except(except).ToArray();
+
+            Console.WriteLine(newText);
+        }
+    }
+}
---
+++ Task15_1_5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task15_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var softwareManufacturers = new List<string>()
+            {
+               "Microsoft", "Apple", "Oracle"
+            };
+
+            var hardwareManufacturers = new List<string>()
+            {
+               "Apple", "Samsung", "Intel"
+            };
+
+            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+
+            foreach (var it in itCompanies)
+            {
+                Console.WriteLine(it);
+            }
+        }
+    }
+}
