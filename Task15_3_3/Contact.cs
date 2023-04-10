ba86383c855c4a7dfd1c4e18a862ba4dffb11590
visot <visotskiy.alexey@gmail.com>
2023-04-10 21:22:12 +0700

    Задание 15.3.3

---
+++ Task15_3_3/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace Task15_3_3
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }   
+
+
+        public Contact(string name, long phone, string email)
+        {
+            this.Name = name;
+            this.Phone = phone;
+            this.Email = email;
+        }
+    }
+}
---
+++ Task15_3_3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task15_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", 799900000013, "serg@gmail.com"));
+            phoneBook.Add(new Contact("Иннокентий", 799900000013, "innokentii@example.com"));
+
+            var emailGroups = phoneBook.GroupBy(contact => contact.Email.Split('@').Last());
+
+            foreach(var group in emailGroups)
+            {
+                if(group.Key.Contains("example"))
+                {
+                    Console.WriteLine("Некорректные адреса");
+                    foreach(var contact in group)
+                        Console.WriteLine(contact.Email);
+                }
+                else
+                {
+                    Console.WriteLine("Настоящие адреса");
+                    foreach (var contact in group)
+                        Console.WriteLine(contact.Email);
+                }
+            }
+
+        }
+    }
+}
