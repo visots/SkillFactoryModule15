8e9c91f5789941c5fd920c16f38bc305cba89ba6
visot <visotskiy.alexey@gmail.com>
2023-04-10 22:25:43 +0700

    Задание 15.5.4

---
+++ Task15_5_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task15_5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Вася", "Петя", "Даша", "Гриша" };
+
+            var withoutPetya = names.Where(n => n.Contains('а'))/*.ToArray()*/;
+
+            names.Add("Таня");
+
+            Console.WriteLine("Без Пети весело:");
+            foreach (var name in withoutPetya)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Всем весело без Пети");
+        }
+    }
+}
