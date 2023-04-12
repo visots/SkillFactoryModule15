9d2e53498e9b26210ef932a110b2a0970b1a815d
visot <visotskiy.alexey@gmail.com>
2023-04-12 22:00:01 +0700

    Итоговое задание 15.6.6

---
+++ FinalTask15_6_6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FinalTask15_6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes) =>(from classroom in classes 
+                                                               from student in classroom.Students 
+                                                               select student).ToArray();
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
